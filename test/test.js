a3610a51976f53adb7900e46e0b70740f67b2904
Siedrix <Siedrix@gmail.com>
2013-11-20 00:38:17 -0600

    Added Model.fetch

--- test/test.js
+++ test/test.js
@@ -20,8 +20,23 @@ var data = [
 	{ type : 'put', key: 'Peyton Manning',		value : { name : 'Peyton Manning'   ,completed : 286,attempts : 409,tds : 34,team : 'DEN' ,active : true  } },
 	{ type : 'put', key: 'Drew Brees',			value : { name : 'Drew Brees'       ,completed : 277,attempts : 406,tds : 26,team : 'NO'  ,active : true  } },
 	{ type : 'put', key: 'Matthew Stafford',	value : { name : 'Matthew Stafford' ,completed : 248,attempts : 419,tds : 21,team : 'DET' ,active : true  } },
-	{ type : 'put', key: 'Aaron Rodgers,',		value : { name : 'Aaron Rodgers,'   ,completed : 168,attempts : 251,tds : 15,team : 'GB'  ,active : false } },
+	{ type : 'put', key: 'Aaron Rodgers',		value : { name : 'Aaron Rodgers'   ,completed : 168,attempts : 251,tds : 15,team : 'GB'  ,active : false } },
 ];
+
+before(function(done){
+	var exampleCollection = new ExampleCollection();
+	exampleCollection.nuke(function (err) {
+		if(err){
+			console.log('Couln\'t nuke Db', err);
+			done(err);
+			return;
+		}
+
+		exampleCollection._db.batch(data, function () {
+			done(err);
+		});
+	});
+});
 
 describe('Backbone set up', function(){
 	describe('Backbone.db', function(){
@@ -60,21 +75,6 @@ describe('Backbone Collection', function(){
 	});
 
 	describe('new ExampleCollection', function () {
-		before(function(done){
-			var exampleCollection = new ExampleCollection();
-			exampleCollection.nuke(function (err) {
-				if(err){
-					console.log('Couln\'t nuke Db', err);
-					done(err);
-					return;
-				}
-
-				exampleCollection._db.batch(data, function () {
-					done(err);
-				});
-			});
-		});
-
 		it('should be a Backbone Collection instance', function () {
 			var exampleCollection = new ExampleCollection();
 
@@ -115,6 +115,80 @@ describe('Backbone Collection', function(){
 					done();
 				}else{
 					done('incorrect length of collection: ' + exampleCollection.length);
+				}
+			});
+		});
+	});
+});
+
+describe('Backbone Models', function(){
+	describe('Backbone.Models basics', function(){
+		it('should have the adapter extend,not the regular extend', function () {
+			assert.equal(typeof Backbone.Model.oldExtend,'function');
+			assert.notEqual(Backbone.Model.oldExtend, Backbone.Collection.extend);
+		});
+	});
+
+	describe('ExampleModel', function () {
+		it('should be a Backbone.Model', function () {
+			// Use this method to test that Example Collection inherits from Backbone.Collection
+			assert.equal(ExampleModel.__super__, Backbone.Model.prototype);
+		});
+
+		it('should have a dbName and db on constructor', function () {
+			// Test that Example collections has a db
+			assert.equal(ExampleModel.dbName, 'example');
+
+			assert.equal(typeof ExampleModel._db, 'object');
+			assert.equal(typeof ExampleModel._db.put, 'function');
+			assert.equal(typeof ExampleModel._db.get, 'function');
+			assert.equal(typeof ExampleModel._db.del, 'function');
+		});
+
+		it('should have a dbName and db on prototype', function () {
+			// Test that Example collections has a db
+			assert.equal(ExampleModel.prototype.dbName, 'example');
+
+			assert.equal(typeof ExampleModel.prototype._db, 'object');
+			assert.equal(typeof ExampleModel.prototype._db.put, 'function');
+			assert.equal(typeof ExampleModel.prototype._db.get, 'function');
+			assert.equal(typeof ExampleModel.prototype._db.del, 'function');
+		});
+	});
+
+	describe('new ExampleModel', function () {
+		it('should be a Backbone Model instance', function () {
+			var exampleModel = new ExampleModel();
+
+			assert.equal(exampleModel.isModel, true);
+		});
+
+		it('should have a _db property and a dbName', function () {
+			var exampleModel = new ExampleModel();
+			assert.equal(exampleModel.dbName, 'example');
+
+			assert.equal(typeof exampleModel._db, 'object');
+			assert.equal(typeof exampleModel._db.put, 'function');
+			assert.equal(typeof exampleModel._db.get, 'function');
+			assert.equal(typeof exampleModel._db.del, 'function');
+		});
+
+		it('#Model.fetch() should get a model by key[Callback]', function (done) {
+			ExampleModel.fetch('Aaron Rodgers', function(err, model){
+				if(err){
+					done(err);
+					return;
+				}
+
+				if(
+					model.isModel &&
+					model.get('id') === 'Aaron Rodgers' &&
+					model.get('name') === 'Aaron Rodgers' &&
+					model.get('team') === 'GB'
+				){
+					done();
+				}else{
+					done('Not the model you are looking for.'+ JSON.stringify( model.toJSON() ) );
 				}
 			});
 		});
